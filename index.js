822eec3894a2a08cf1ed33c9dcf168680c8c8bff
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-21 21:10:23 +0530

    added text file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("welcome to js");
+const {EventEmitter} = require('events');
+const {readFile, readFileSync} = require('fs');
+
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on("end", () => {
+    console.log("wattup broski");
+})
+
+eventEmitter.emit("end");
+console.log("second");
