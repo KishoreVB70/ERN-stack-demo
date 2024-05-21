f7bfc66f2a30f776f126ddf35f54a3daedfa6fe1
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-21 21:25:18 +0530

    added promise based function

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const {EventEmitter} = require('events');
-const {readFile} = require('fs');
+// const {readFile} = require('fs');
+const {readFile} = require('fs').promises;
 
 const eventEmitter = new EventEmitter();
 
 // The call back is run only after the text file is read
-readFile("./hello.txt", "utf8", (err, data) => {
-    console.log(data);
-})
+// readFile("./hello.txt", "utf8", (err, data) => {
+//     console.log(data);
+// })
+
+async function getter() {
+    const content = await readFile("./hello.txt", "utf8");
+    console.log(content);
+}
+
+getter();
 
 console.log("second");
 eventEmitter.on("end", () => {
