dc33b08ae8b88c4490785a38e7ae82a81c37a3b3
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-21 21:18:02 +0530

    added async readFile

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const txt = readFileSync("./hello.txt", "utf8");
 
 console.log(txt);
 
+readFile("./hello.txt", "utf8", (err, data) => {
+    console.log(data);
+})
+
+console.log("second");
 eventEmitter.on("end", () => {
     console.log("wattup broski");
 })
