1ac8ebf2085a3ac3e1f1cbf5aeab057093ea7e6f
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-21 21:16:43 +0530

    added readfile sync

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const {readFile, readFileSync} = require('fs');
 
 const eventEmitter = new EventEmitter();
 
+const txt = readFileSync("./hello.txt", "utf8");
+
+console.log(txt);
+
 eventEmitter.on("end", () => {
     console.log("wattup broski");
 })
 
 eventEmitter.emit("end");
-console.log("second");
