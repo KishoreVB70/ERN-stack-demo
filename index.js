0cbfe0f8fec4446edfd9e4874cc88d8bee2c0d0c
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-22 08:00:47 +0530

    added express app

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const {EventEmitter} = require('events');
+const express = require('express');
 // const {readFile} = require('fs');
 const {readFile} = require('fs').promises;
 
 const eventEmitter = new EventEmitter();
-
+const app = express();
+app.get("/", () => {
+    
+})
 // The call back is run only after the text file is read
 // readFile("./hello.txt", "utf8", (err, data) => {
 //     console.log(data);
